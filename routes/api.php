4644c816927d691ae4111ea9653fec81041704c3
Riop7 <if20.riopratama@mhs.ubpkarawang.ac.id>
2022-04-12 11:50:19 +0700

    pengerjaan UTS PWF

---
+++ routes/api.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProdukController;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+Route::post('/product', [ProdukController::class, 'store']);
+Route::get('/product', [ProdukController::class, 'show']);
+Route::get('/product/{id}', [ProdukController::class, 'showById']);
+Route::put('/product/{id}',[ProdukController::class, 'update']);
+Route::delete('/product/{id}', [ProdukController::class, 'delete']);
+
